c0d304df57d24996913c24da6c07d9df2ab888d9
basmallasaid <basmala30311@gmail.com>
2026-05-09 15:01:07 +0300

    delete

--- app/api/bookings/[id]/route.js
+++ app/api/bookings/[id]/route.js
@@ -3,17 +3,21 @@ import { NextResponse } from "next/server";
 const MOCK_API = process.env.NEXT_PUBLIC_APIURLBOOKINGS;
 
 export async function DELETE(request, { params }) {
-  const { id } = params;
+  const { id } = await params; 
   try {
-    await fetch(`${MOCK_API}/${id}`, { method: "DELETE" });
+    const res = await fetch(`${MOCK_API}/${id}`, { method: "DELETE" });
+    if (!res.ok) throw new Error("Failed to delete from MockAPI");
+    
     return NextResponse.json({ message: "Deleted from MockAPI" });
   } catch (error) {
+    console.error("Delete error:", error);
     return NextResponse.json({ message: "Delete failed" }, { status: 500 });
   }
 }
 
 export async function PUT(request, { params }) {
-  const { id } = params;
+  const { id } = await params; 
+
   try {
     const updatedData = await request.json();
     const res = await fetch(`${MOCK_API}/${id}`, {
@@ -21,9 +25,13 @@ export async function PUT(request, { params }) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(updatedData),
     });
+
+    if (!res.ok) throw new Error("Failed to update MockAPI");
+
     const data = await res.json();
     return NextResponse.json(data);
   } catch (error) {
+    console.error("Update error:", error);
     return NextResponse.json({ message: "Update failed" }, { status: 500 });
   }
 }
